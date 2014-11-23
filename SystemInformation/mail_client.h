c7ea45fae7cc88b5f918bf650449deb4b27532bc
zcfelix <zcfelix.zhou@gmail.com>
2014-11-23 19:24:34 +0800

    Backup point 1

--- SystemInformation/mail_client.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MAIL_CLIENT_H
-#define __MAIL_CLIENT_H
-
-#include "software_info.h"
-int CreateMailClientList(std::vector<SoftwareInfo> *p_mc_list);
-bool IsMailClient(SoftwareInfo *p_s_info);
-#endif
